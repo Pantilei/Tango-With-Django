1e9b2cc64bb0e030d95b7fd0d63e066312ac48d9
UtenteGen5 <UtenteGen5@Frigelccs.local>
2018-11-05 16:49:50 +0100

    First commit of initial web with about and home pages and links

---
+++ rango/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse("Rango says: Hello world! <br/> <a href='/rango/about/'>About</a>")
+def about(request):
+    return HttpResponse("Rango says: Here is the About page <br/> <a href='/rango/'>Index</a>")
