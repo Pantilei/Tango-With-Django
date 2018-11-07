cbb36e84754ed86c768a7e1604514af294a761ae
UtenteGen5 <UtenteGen5@Frigelccs.local>
2018-11-07 10:39:32 +0100

    Templates folder and first picture were added

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 def index(request):
-    return HttpResponse("Rango says: Hello world! <br/> <a href='/rango/about/'>About</a>")
+    context_dict={'boldmessage':"Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request,'rango/index.html', context=context_dict)
+    #return HttpResponse("HI bro!!!")
 def about(request):
     return HttpResponse("Rango says: Here is the About page <br/> <a href='/rango/'>Index</a>")
