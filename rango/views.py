5d01dd61082798459bfcade68580f6d8df120a9a
UtenteGen5 <UtenteGen5@Frigelccs.local>
2018-11-27 16:05:09 +0100

    Cookies and Sessions

--- rango/views.py
+++ rango/views.py
@@ -8,16 +8,50 @@ from rango.models import Page
 from rango.forms import CategoryForm
 from rango.forms import PageForm
 from rango.forms import UserForm, UserProfileForm
+from datetime import datetime
 
 def index(request):
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages':page_list}
-    return render(request,'rango/index.html', context_dict)
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    context_dict['last_visit'] = request.session['last_visit']
+
+    response = render(request,'rango/index.html', context=context_dict)
+    return response
     #return HttpResponse("HI bro!!!")
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request,'visits','1'))
+    last_visit_cookie = get_server_side_cookie(request,'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).seconds > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+    request.session['visits'] = visits
+
 def about(request):
+    #if request.session.test_cookie_worked():
+        #print ("TEST COOKIE WORKED!")
+        #request.session.delete_test_cookie()
     output={'message':"This page has been put together by Pantilei"}
-    return render(request,"rango/about.html",context=output)
+    visitor_cookie_handler(request)
+    output['last_visit'] = request.session['last_visit']
+    output['visits'] = request.session['visits']
+    response = render(request,"rango/about.html",context=output)
+    return response
 
 def show_category(request, category_name_slug):
     context_dict = {}
